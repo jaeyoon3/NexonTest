af89c6089b2c41e6d8339f3527b0df2878d7b8ce
jaeyoon3 <wodbs3191@naver.com>
2025-05-18 20:59:16 +0900

    docker?

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  throw new Error('MONGODB_URI 환경 변수가 설정되지 않았습니다.');
+}
 
 @Module({
   imports: [
-    MongooseModule.forRoot('uri'), // DB 연결
+    MongooseModule.forRoot(mongoUri), // DB 연결
     AuthModule,
   ],
 })
--- apps/event/src/event.module.ts
+++ apps/event/src/event.module.ts
@@ -7,11 +7,20 @@ import { Condition, ConditionSchema } from './schema/condition.schema';
 import { EventController } from './event.controller';
 import { EventService } from './event.service';
 import { HttpModule } from '@nestjs/axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  throw new Error('MONGODB_URI 환경 변수가 설정되지 않았습니다.');
+}
 
 @Module({
   imports: [
     HttpModule,
-    MongooseModule.forRoot('uri'),
+    MongooseModule.forRoot(mongoUri),
     MongooseModule.forFeature([
       { name: Event.name, schema: EventSchema },
       { name: Reward.name, schema: RewardSchema },
